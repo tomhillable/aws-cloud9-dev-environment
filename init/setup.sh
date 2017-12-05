7d76acae907f3b1330cd4845a1aeb07f0b678ca2
Tom Hill <tom@clockworkcubed.com>
2017-12-05 18:48:02 +0000

    Shallow clone git repos

--- init/setup.sh
+++ init/setup.sh
@@ -7,12 +7,15 @@ fi
 
 source ${HOME}/environment/python3/bin/activate
 
-pip install --upgrade -r ${HOME}/environment/aws-cloud9-dev-environment/init/requirements.txt
+pip install --upgrade -r "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/requirements.txt
 sudo yum -y install jq
 git config --global credential.helper parameter-store
 
 github_user=$(echo 'host=github.com' | git-credential-parameter-store get | sed -ne 's/^username=//p')
 github_pass=$(echo 'host=github.com' | git-credential-parameter-store get | sed -ne 's/^password=//p')
+
+git config --global user.name "$(curl -su ${github_user}:${github_pass} 'https://api.github.com/user' | jq -r '.name')"
+git config --global user.email "$(curl -su ${github_user}:${github_pass} 'https://api.github.com/user' | jq -r '.email')"
 
 for page in $(seq 1 10)
 do
@@ -20,7 +23,7 @@ do
     do
         if [ ! -d "${HOME}/environment/${repo}" ]; then
             echo "Cloning repo ${repo}"
-            git clone https://github.com/${repo}.git ${HOME}/environment/${repo}
+            git clone --depth 1 https://github.com/${repo}.git ${HOME}/environment/github.com/${repo}
         fi
     done
 done
