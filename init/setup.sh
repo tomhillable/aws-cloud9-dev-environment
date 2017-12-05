aff67a7610247f03553cc977e25f1a54fcd93b4d
Tom Hill <tom@clockworkcubed.com>
2017-12-05 18:19:44 +0000

    Pull list of github repos automatically

--- init/setup.sh
+++ init/setup.sh
@@ -8,19 +8,19 @@ fi
 source ${HOME}/environment/python3/bin/activate
 
 pip install --upgrade -r ${HOME}/environment/aws-cloud9-dev-environment/init/requirements.txt
+sudo yum -y install jq
 git config --global credential.helper parameter-store
 
-repos=(
-    "aws-datapipeline-s3-copy"
-    "alice"
-    "dockerfiles"
-    "git-credential-parameter-store"
-)
+github_user=$(echo 'host=github.com' | git-credential-parameter-store get | sed -ne 's/^username=//p')
+github_pass=$(echo 'host=github.com' | git-credential-parameter-store get | sed -ne 's/^password=//p')
 
-for repo in "${repos[@]}"
+for page in $(seq 1 10)
 do
-    if [ ! -d "${HOME}/environment/${repo}" ]; then
-        echo "Cloning repo ${repo}"
-        git clone https://github.com/tomhillable/${repo}.git ${HOME}/environment/${repo}
-    fi
+    for repo in $(curl -s -u ${github_user}:${github_pass} "https://api.github.com/user/repos?per_page=200&page=${page}" | jq -r '.[].full_name')
+    do
+        if [ ! -d "${HOME}/environment/${repo}" ]; then
+            echo "Cloning repo ${repo}"
+            git clone https://github.com/${repo}.git ${HOME}/environment/${repo}
+        fi
+    done
 done
